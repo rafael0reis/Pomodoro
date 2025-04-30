60502e56a84ccdca1f73a3648bbeea3cba536fa9
Rafael Reis <rafaelrg0705@gmail.com>
2025-04-30 15:46:32 -0300

    melhorando funções do projeto

--- script.js
+++ script.js
@@ -21,69 +21,104 @@ var faseCumpridaEl = document.querySelector('#faseCumprida')
 
 var intervalTimer
 var verificacao = false
-var incSeg = 60
-var incMinPom = 25
+var tempos = {
+    incSeg: 60,
+    incMinPom: 25,
+    minTempoC: 5,
+    minTempoL: 15
+}
 
 
 //eventos dinâmicos 
 
 //botões
-bttComecarEl.addEventListener('click', iniciarContador)
+bttComecarEl.addEventListener('click', ()=>{iniciarPom()})
 bttPausarEl.addEventListener('click', pausarContador)
 bttContinuarEl.addEventListener('click', continuarContador)
 
 //fases
+faseCurtaEl.addEventListener('click', pausaCurta)
+faseProduzirEl.addEventListener('click', faseProduzir)
 
 
-// funções de funcionamento
 
+//funções de funcionamento
 
-function iniciarContador(){
-  
-    incSeg --
-    incMinPom --
-    bttComecarEl.style.display = 'none'
-    bttPausarEl.style.display = 'block'
-    bttPausarEl.style.boxShadow = 'none'
-    bttPausarEl.style.height = '65px'
-    bttPausarEl.style.marginTop = '10px'
-    mintEl.textContent = `${incMinPom}`
-    segdEl.textContent = `${incSeg}`
+//função que vai diminuir com base nos valores que receber, e vai atualizar na tela em tempo real
+function contador(mint, segd){
 
     intervalTimer = setInterval(()=>{
-        if(!verificacao){
-            incSeg--
-            segdEl.textContent = `${formatTimer(incSeg)}`
-            if(incMinPom > 0){
-                if(incSeg == 0){
-                    incSeg = 60
-                    incMinPom--
-                    mintEl.textContent = `${formatTimer(incMinPom)}`
-                }
-            }else if(incMinPom == 0 & incSeg == 0){
-                clearInterval(intTimer)
-            }
-        }
-
+        
     }, 1000)
 }
 
+function iniciarPom(){
+    contador(tempos.incMinPom, tempos.incSeg)
+
+    // verificacao = verify
+    
+    // bttComecarEl.style.display = 'none'
+    // bttContinuarEl.style.display = 'none'
+    
+    // bttPausarEl.style.display = 'block'
+    // bttPausarEl.style.boxShadow = 'none'
+    // bttPausarEl.style.height = '65px'
+    // bttPausarEl.style.marginTop = '10px'
+
+    // if(!verificacao){
+    //     tempos.incSeg --
+    //     tempos.incMinPom --
+        
+    //     mintEl.textContent = `${formatTimer(tempos.incMinPom)}`
+    //     segdEl.textContent = `${tempos.incSeg}`
+    // }
+
+    // intervalTimer = setInterval(()=>{
+    //     if(!verificacao){
+    //         tempos.incSeg--
+    //         segdEl.textContent = `${formatTimer(tempos.incSeg)}`
+    //         if(tempos.incMinPom > 0){
+    //             if(tempos.incSeg == 0){
+    //                 tempos.incSeg = 60
+    //                 tempos.incMinPom--
+    //                 mintEl.textContent = `${formatTimer(tempos.incMinPom)}`
+    //             }
+    //         }else if(tempos.incMinPom == 0 & tempos.incSeg == 0){
+    //             clearInterval(intervalTimer)
+    //         }
+    //     }
+
+    // }, 1000)
+}
 function pausarContador(){
         verificacao = true
         bttPausarEl.style.display   = 'none'
         bttContinuarEl.style.display = 'block'
 }
-
 function continuarContador(){
         verificacao = false
         bttContinuarEl.style.display = 'none'
         bttPausarEl.style.display    = 'block'
 }
 
+//ao clicar no shortbreak
+function pausaCurta(){
+
+    // clearInterval(intervalTimer)
+    // iniciarContador(true)
+    // bttPausarEl.style.display = 'none'
+    // bttComecarEl.style.display = 'block'
+    // bttContinuarEl.style.display = 'none'
+}
+
+function faseProduzir(){
+
+}
+
 
 //funções de apoio
 
-
+//formata números menores que 10 com 0 antes
 function formatTimer(num){
     return num < 10 ? `0${num}` : num
 }
